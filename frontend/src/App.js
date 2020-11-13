381e6af617fe07e8b56cd66b8030638d40140af2
Levy-Naibei <levynaibei2@gmail.com>
2020-11-13 13:55:58 +0300

    feat: initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UserInputs from './components/UserInputs'
+
+const App = () => {
+  return (
+    <div className="App">
+        <UserInputs />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/UserInputs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserInputs = () => {
+    return (
+        <div>
+            <h3>Enter your inputs to be sent to hit ml model</h3>
+        </div>
+    )
+}
+
+export default UserInputs;
