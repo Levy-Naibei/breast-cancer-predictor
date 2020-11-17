bug: fixed encoding, feature scaling

backend/app.py
@@ -14,11 +14,11 @@ def defaultpage():
     return render_template('index.html')
 
 # predict
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     int_features = [float(x) for x in request.form.values()]
     final_features = np.array(int_features)
-    pred = model.predict(final_features)
+    pred = model.predict([final_features])
     return render_template('index.html', results="Breast Cancer group is {}".format(pred))
 
 